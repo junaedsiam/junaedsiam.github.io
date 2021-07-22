db62b2beade39407e51c15502ed9605bb45848b7
junaedsiam <junaedsiam@gmail.com>
2021-07-23 00:24:09 +0600

    type definition procedure update, basic container implemented

---
+++ src/components/container/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ReactChildren } from 'interface'
+
+const Container = ({ children }: ReactChildren) => {
+  return <div>{children}</div>
+}
+
+export default Container
